987cac4b9ac60a10785ee3f424d078b069a86dfe
kurnevsky@gmail.com <kurnevsky@gmail.com@af8f25c2-68d8-5739-eed1-5993eff168c6>
2011-08-04 11:36:15 +0000

    Реализован 6 протокол.

--- trunk/PointsBot/BotEngine.h
+++ trunk/PointsBot/BotEngine.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "Field.h"
+#include "BasicTypes.h"
 
 // Возвращает лучший найденный ход.
 // Если не найдено - 0.
 p_int SearchBestMove(Field &MainField, p_int Depth, p_int UCTIterations);
+
+p_int SearchBestMoveWithTime(Field &MainField, p_int Time);
