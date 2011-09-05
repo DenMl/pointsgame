1169738ec33decf56673b32910f6ac01c0e80382
kurnevsky <kurnevsky@gmail.com>
2011-09-05 20:18:07 +0000

    Удален мусор.

--- PointsBot/Zobrist.cpp
+++ PointsBot/Zobrist.cpp
@@ -1,14 +1,13 @@
 #include "random.h"
 #include "basic_types.h"
-#include "BasicConstants.h"
 
 using namespace std;
 
-ulong Zobrist[2][2][PointsLength22];
+ulong Zobrist[2][2][2000];
 
 void ZobristInit()
 {
-	for (uint i = 0; i < PointsLength22; i++)
+	for (uint i = 0; i < 2000; i++)
 	{
 		Zobrist[0][0][i] = Random();
 		Zobrist[0][1][i] = Random();
