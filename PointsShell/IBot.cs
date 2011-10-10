f67148d6ed7b66f95102c2f98dbd18ddad0ee52b
kurnevsky <kurnevsky@gmail.com>
2011-10-10 18:20:11 +0000

    Добавлен интерфейс, через который в будущем будут работать ии.

---
+++ PointsShell/IBot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PointsShell
+{
+	interface IBot
+	{
+		void PutPoint(Pos pos);
+		void PutPoint(Pos pos, PlayerColor player);
+		void RemoveLastPoint();
+		Pos GetBotMovie(string command);
+		void SetCurrentPlayer(PlayerColor player);
+		PlayerColor GetCurrentPlayer();
+		void SetNextPlayer();
+	}
+}
