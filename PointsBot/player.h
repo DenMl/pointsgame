c0bdb722888b00530d24b4033b2bca3f30a98562
kurnevsky <kurnevsky@gmail.com>
2011-10-10 17:42:45 +0000

    Исправления в используемых типах.

--- PointsBot/player.h
+++ PointsBot/player.h
@@ -1,16 +1,17 @@
 #pragma once
 
 #include "config.h"
+#include "basic_types.h"
 
 using namespace std;
 
 // Значение бита первого игрока.
-const short player_red = 0x0;
+const player player_red = 0x0;
 // Значение бита второго игрока.
-const short player_black = 0x1;
+const player player_black = 0x1;
 
 // Получить по игроку следующего игрока.
-inline short next_player(const short cur_player)
+inline player next_player(const player cur_player)
 {
 	return cur_player ^ 1;
 }
