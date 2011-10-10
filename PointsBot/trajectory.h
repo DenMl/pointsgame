46a98a3eb752df72e3cf90d822d64fe4aaec47f8
kurnevsky <kurnevsky@gmail.com>
2011-10-10 17:53:40 +0000

    Некоторые методы помечены как inline.

--- PointsBot/trajectory.h
+++ PointsBot/trajectory.h
@@ -24,14 +24,14 @@ public:
 	inline void clear() { _points.clear(); _hash = 0; _excluded = false; }
 	inline const trajectory& operator =(const trajectory &other) { _points = other._points; _hash = other._hash; _excluded = other._excluded; _zobrist = other._zobrist; return *this; }
 	inline void swap(trajectory &other) { trajectory tmp(*this); *this = other; other = tmp; }
-	list<pos>::iterator begin() { return _points.begin(); }
-	list<pos>::const_iterator begin() const { return _points.begin(); }
-	list<pos>::iterator end() { return _points.end(); }
-	list<pos>::const_iterator end() const { return _points.end(); }
-	list<pos>::reverse_iterator rbegin() { return _points.rbegin(); }
-	list<pos>::reverse_iterator rend() { return _points.rend(); }
-	list<pos>::const_reverse_iterator rbegin() const { return _points.rbegin(); }
-	list<pos>::const_reverse_iterator rend() const { return _points.rend(); }
+	inline list<pos>::iterator begin() { return _points.begin(); }
+	inline list<pos>::const_iterator begin() const { return _points.begin(); }
+	inline list<pos>::iterator end() { return _points.end(); }
+	inline list<pos>::const_iterator end() const { return _points.end(); }
+	inline list<pos>::reverse_iterator rbegin() { return _points.rbegin(); }
+	inline list<pos>::reverse_iterator rend() { return _points.rend(); }
+	inline list<pos>::const_reverse_iterator rbegin() const { return _points.rbegin(); }
+	inline list<pos>::const_reverse_iterator rend() const { return _points.rend(); }
 
 	template<typename _InIt>
 	inline void assign(_InIt first, _InIt last)
