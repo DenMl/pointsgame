7334b376e3c116f271168871e4b2aa7aa3d854d0
kurnevsky <kurnevsky@gmail.com>
2011-10-10 17:23:51 +0000

    Комментарии.

--- PointsBot/zobrist.h
+++ PointsBot/zobrist.h
@@ -6,32 +6,39 @@
 
 using namespace std;
 
+// Хеш Зобриста.
 class zobrist
 {
 private:
+	// Размер таблицы хешей.
 	size_t _size;
-	size_t* _hashes;
+	// Хеши.
+	hash_t *_hashes;
 
 public:
+	// Конструктор.
 	inline zobrist(size_t size)
 	{
 		_size = size;
-		_hashes = new size_t[size];
+		_hashes = new hash_t[size];
 		Randomize();
 		for (size_t i = 0; i < size; i++)
 			_hashes[i] = Random();
 	}
+	// Конструктор копирования.
 	inline zobrist(const zobrist &other)
 	{
 		_size = other._size;
-		_hashes = new size_t[other._size];
+		_hashes = new hash_t[other._size];
 		copy_n(other._hashes, other._size, _hashes);
 	}
+	// Деструктор.
 	inline ~zobrist()
 	{
 		delete _hashes;
 	}
-	inline size_t get_hash(size_t num)
+	// Возвращает хеш Зобриста элемента num.
+	inline hash_t get_hash(size_t num)
 	{
 		return _hashes[num];
 	}
