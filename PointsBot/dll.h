7fabd58e929ad8be9670b2cc5b288b0e0a4e3e03
kurnevsky <kurnevsky@gmail.com>
2011-12-30 11:13:19 +0000

    Рефакторинг.

--- PointsBot/dll.h
+++ PointsBot/dll.h
@@ -1,16 +1,16 @@
 #pragma once
 
 #include "config.h"
-#include "field.h"
+#include "bot.h"
 
 #define DLLEXPORT extern "C" __declspec(dllexport)
 
-DLLEXPORT field* init(int width, int height);
-DLLEXPORT void final(field* cur_field);
-DLLEXPORT void put_point(field* cur_field, int x, int y, int cur_player);
-DLLEXPORT void remove_last_point(field* cur_field);
-DLLEXPORT void get_move(field* cur_field, int& x, int& y, int cur_player);
-DLLEXPORT void get_move_with_complexity(field* cur_field, int& x, int& y, int cur_player, int complexity);
-DLLEXPORT void get_move_with_time(field* cur_field, int& x, int& y, int cur_player, int time);
+DLLEXPORT bot* init(coord width, coord height);
+DLLEXPORT void final(bot* cur_bot);
+DLLEXPORT void put_point(bot* cur_bot, coord x, coord y, player cur_player);
+DLLEXPORT void remove_last_point(bot* cur_bot);
+DLLEXPORT void get_move(bot* cur_bot, coord& x, coord& y, player cur_player);
+DLLEXPORT void get_move_with_complexity(bot* cur_bot, coord& x, coord& y, player cur_player, int complexity);
+DLLEXPORT void get_move_with_time(bot* cur_bot, coord& x, coord& y, player cur_player, int time);
 DLLEXPORT wchar_t* get_name();
 DLLEXPORT wchar_t* get_version();
